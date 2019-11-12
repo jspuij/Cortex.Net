eb2472631a0dd194e6f6cf402e335a0113433dac
Jan-Willem Spuij <jw@spuij.nl>
2019-11-12 11:36:46 +0100

    Start on TodoMVC sample. References #10.

---
+++ samples/Cortex.Net.BlazorTodo/webpack.config.js
@@ -0,0 +1,35 @@
+﻿const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry:  [
+            './node_modules/todomvc-common/base.js',
+            './node_modules/todomvc-common/base.css',
+            './node_modules/todomvc-app-css/index.css'
+        ]
+,
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, './wwwroot/js'),
+        filename: "app.js",
+        library: "cortex_net_blazortodo"
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/style.css'
+        })
+    ]
+};
