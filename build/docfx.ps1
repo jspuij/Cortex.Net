90234880e2acf66fc48b0f780d71e5ee194aa90b
Jan-Willem Spuij <jw@spuij.nl>
2019-11-15 19:57:19 +0100

    Test build

--- build/docfx.ps1
+++ build/docfx.ps1
@@ -1,6 +1,5 @@
 git config --global user.email "$Env:GH_EMAIL"
 git config --global user.name "$Env:GH_USER"
 cd docs
-echo "https://jspuij:$Env:BUILD_TOKEN@github.com/jspuij/Cortex.Net.Docs.git"
 git submodule add -f --name Cortex.Net.Docs "https://jspuij:$Env:BUILD_TOKEN@github.com/jspuij/Cortex.Net.Docs.git" "_site"
 dir
