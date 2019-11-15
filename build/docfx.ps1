3e348102034ecc6e757a4c7a945fb565d7abcd25
Jan-Willem Spuij <jw@spuij.nl>
2019-11-15 19:19:46 +0100

    Add token to submodule add

--- build/docfx.ps1
+++ build/docfx.ps1
@@ -1,5 +1,5 @@
 git config --global user.email "$Env:GH_EMAIL"
 git config --global user.name "$Env:GH_USER"
 cd docs
-git submodule add -f --name Cortex.Net.Docs "https://github.com/jspuij/Cortex.Net.Docs" "_site"
+git submodule add -f --name Cortex.Net.Docs "https://jspuij:$Env:BUILD_TOKEN@github.com/jspuij/Cortex.Net.Docs" "_site"
 dir
