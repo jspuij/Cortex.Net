6d97943bb35275d05cdc8a674d7c56d8025757f8
Jan-Willem Spuij <jw@spuij.nl>
2019-11-15 21:31:12 +0100

    Updated build script

--- build/docfx.ps1
+++ build/docfx.ps1
@@ -1,5 +1,10 @@
 cd docs
 dir
 docfx docfx.json
-cd \a\
-dir
+cd ..\..\Cortex.Net.Docs\
+git checkout master
+cd ..\Cortex.Net\docs\
+xcopy .\_site\*.* ..\..\Cortex.Net.Docs\ /Y /E
+cd ..\..\Cortex.Net.Docs\
+git commit -m "Automated Documentation."
+git push
