de72c9858b368717e7a98e1187a15faa7c89678d
Jan-Willem Spuij <jw@spuij.nl>
2019-11-15 22:16:08 +0100

    Push action.

--- build/docfx.cmd
+++ build/docfx.cmd
@@ -10,4 +10,3 @@ xcopy .\_site\*.* ..\..\Cortex.Net.Docs\ /Y /E
 cd ..\..\Cortex.Net.Docs\
 git add .
 git commit -m "Automated Documentation."
-git -c http.extraheader="AUTHORIZATION: basic %token%" push --progress
