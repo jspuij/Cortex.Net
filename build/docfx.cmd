72014f71f7a2defd0f2b19a435965bdd95ce515f
Jan-Willem Spuij <jw@spuij.nl>
2019-11-15 21:52:34 +0100

    Try with bat file.

--- build/docfx.cmd
+++ build/docfx.cmd
@@ -6,5 +6,6 @@ git checkout master
 cd ..\Cortex.Net\docs\
 xcopy .\_site\*.* ..\..\Cortex.Net.Docs\ /Y /E
 cd ..\..\Cortex.Net.Docs\
+git add .
 git commit -m "Automated Documentation."
-git -c http.extraheader="AUTHORIZATION: basic $Env:token" push --progress
+git -c http.extraheader="AUTHORIZATION: basic %token%" push --progress
